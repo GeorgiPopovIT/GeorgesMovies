516140580a96162e14d0a7b4ba8f59adbb939a58
GeorgiPopovIT <gogopopo26062003@gmail.com>
2021-06-21 17:22:48 +0300

    User and Comment tables.

---
+++ GeorgesMovies/GeorgesMovies.Models/Models/User.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Collections.Generic;
+
+namespace GeorgesMovies.Models.Models
+{
+    public class User
+    {
+        public User()
+        {
+            this.Movies = new HashSet<Movie>();
+            this.Comments = new HashSet<Comment>();
+        }
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        public ICollection<Movie> Movies { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+    }
+}
